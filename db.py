9406f9c36f18f602ab59bac36051bc01e2db2335
Sam Higgs <samhiggs@users.noreply.github.com>
2021-07-30 18:05:03 +1000

    first commit

---
+++ db.py
@@ -0,0 +1,91 @@
+import sqlite3, random
+import pandas as pd
+
+DBNAME='winereviews.db'
+
+class SQLite:
+    """Example of a contextmanager"""
+
+    def __init__(self, file='winereviews.db'):
+        self.file = file
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.file)
+        return self.conn.cursor()
+    def __exit__(self, tpye, value, traceback):
+        self.conn.close()
+
+# Example of creating a custom exception name
+class NotFoundError(Exception):
+    pass
+
+class NotAuthorizedError(Exception):
+    pass
+
+# You can also use decorators for retrying for the case where it might just
+# be a random issue and retrying is solved.
+
+# You can also create custom logging for exceptions with a logger.
+
+def review_lst_to_json(item):
+    return {
+        "id": item[0],
+        "country": item[1],
+        "description": item[2],
+        "designation": item[3],
+        "points": item[4],
+        "price": item[5],
+        "province": item[6],
+        "region_1": item[7],
+        "region_2": item[8],
+        "variety": item[9],
+        "winery": item[10],
+        "private": item[11],
+        "user_published": item[12]
+    }
+
+def fetch_reviews(user: str = None):
+    try:
+        with SQLite('winereviews.db') as cur:
+            # If user is none select 20 from a random start, otherwise only get users
+            if user is None:
+                qry = f'SELECT * FROM reviews where private=0 limit 20 offset {random.randint(0,10000)}'
+            else:
+                qry = f'SELECT * FROM reviews where private=0 and user_published = {user} limit 20'
+            cur.execute(qry)
+            result = list(map(review_lst_to_json, cur.fetchall()))
+            return result
+
+    except Exception as e:
+        print(e)
+        return []
+
+def fetch_user_reviews(user: str):
+    df = pd.DataFrame(fetch_reviews(user))
+    df = df[df['user_published'] == user]
+    return df.to_html()
+
+def fetch_review(id: str):
+
+    try:
+        con = sqlite3.connect(DBNAME)
+        cur = con.cursor()
+
+        cur.execute(f"SELECT * FROM reviews where id={id}")
+        result = cur.fetchone()
+
+        if result is None:
+            raise NotFoundError(f'Unable to find review with id {id}.')
+
+        data = review_lst_to_json(result)
+
+        if data['private']:
+            raise NotAuthorizedError(f'You are not allowed to access review with id {id}')
+
+        con.close()
+
+        return data
+    except sqlite3.OperationalError as e:
+        print(e)
+        raise NotFoundError(f'Unable to find review with id {id}.')
+    finally:
+        con.close()
---
+++ wine-api.py
@@ -0,0 +1,68 @@
+import yaml, os
+import flask
+from flask import Flask, jsonify, abort, session
+import requests_oauthlib
+from db import fetch_reviews, fetch_review, fetch_user_reviews, NotFoundError, NotAuthorizedError
+
+app = Flask(__name__)
+
+config = yaml.safe_load(open('configuration.yml', 'r'))
+CLIENT_ID = config['simplelogin']['CLIENT_ID']
+CLIENT_SECRET = config['simplelogin']['CLIENT_SECRET']
+
+AUTHORIZATION_BASE_URL = 'https://app.simplelogin.io/oauth2/authorize'
+TOKEN_URL = 'https://app.simplelogin.io/oauth2/token'
+USERINFO_URL = 'https://app.simplelogin.io/oauth2/userinfo'
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+@app.route('/')
+def index():
+    return '''
+    <a href="/login">Login</a>
+    '''
+
+@app.route('/login')
+def login():
+    simplelogin = requests_oauthlib.OAuth2Session(
+        CLIENT_ID,
+        redirect_uri='http://localhost:5000/callback'
+    )
+    authorization_url, state = simplelogin.authorization_url(AUTHORIZATION_BASE_URL)
+    return flask.redirect(authorization_url)
+
+@app.route('/callback')
+def callback():
+    simplelogin = requests_oauthlib.OAuth2Session(CLIENT_ID)
+    simplelogin.fetch_token(
+        TOKEN_URL,
+        client_secret=CLIENT_SECRET,
+        authorization_response=flask.request.url
+    )
+
+    user_info = simplelogin.get(USERINFO_URL).json()
+    return f"""
+    User information: <br>
+    Name: {user_info["name"]} <br>
+    Email: {user_info["email"]} <br>
+    Avatar <img src="{user_info.get('avatar_url')}"> <br>
+    <br>
+    {fetch_user_reviews(user_info["email"])} <br><br>
+    <a href="/">Home</a>
+    """
+
+@app.route('/reviews')
+def all_reviews():
+    return jsonify(fetch_reviews())
+
+@app.route('/review/<id>')
+def get_reveiew(id):
+    try:
+        return jsonify(fetch_review(id))
+    except NotFoundError:
+        abort(404, description='Resource not found')
+    except NotAuthorizedError:
+        abort(403, description='Access Denied')
+
+if __name__ == '__main__':
+    app.run(debug=True)
